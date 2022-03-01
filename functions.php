8ff4527d891e9ef468cd679735a3c674090ea938
Doug <karaballo.douglas@hotmail.com>
2022-03-01 10:37:13 -0400

    Comienzo del proyecto y modificaciones al header

--- functions.php
+++ functions.php
@@ -140,10 +140,13 @@ add_action( 'widgets_init', 'hechizandotheme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function hechizandotheme_scripts() {
-	wp_enqueue_style( 'hechizandotheme-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'hechizandotheme-style', 'rtl', 'replace' );
+	//wp_enqueue_style( 'hechizandotheme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	//wp_style_add_data( 'hechizandotheme-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'hechizandotheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	//wp_enqueue_script( 'hechizandotheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	wp_enqueue_script('tema-script',get_template_directory_uri().'/public/build/index.js',array(),'1.0.0',true);
+	wp_enqueue_style('miestilo', get_template_directory_uri().'/public/build/index.css',array(),'1.0.0');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ template-parts/components/reusable/element-logo.php
@@ -0,0 +1,5 @@
+<div class="container-logo">
+    <a href="<?= home_url('/'); ?>">
+        <img class= "container-logo__img" src="<?= get_template_directory_uri().'/dist/assets/img/logo.png' ;?>" alt="Hechizando la cocina">
+    </a>
+</div>
