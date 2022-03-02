df3fde6b40b6be220ab7a7e051f37b2f92365c15
Doug <karaballo.douglas@hotmail.com>
2022-03-02 07:57:27 -0400

    Creacion del menu en el header y del search

---
+++ template-parts/components/header/element-menu.php
@@ -0,0 +1,11 @@
+<div class="header-menu">
+    <?php
+        $args = [
+            'menu'=>'Menu 1',
+            'menu_class'=> 'menu-nav-desktop',
+            'container'=> '',
+        ];
+
+        wp_nav_menu($args);
+    ?>
+</div>
