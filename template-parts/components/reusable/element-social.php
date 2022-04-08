ab18812a0c3ba40bc8871815daab95226f97b47c
ksilveira <silveirak908@gmail.com>
2022-04-08 11:14:19 -0400

    modificaciones al footer

--- template-parts/components/reusable/element-social.php
+++ template-parts/components/reusable/element-social.php
@@ -1,5 +1,5 @@
 <ul class="social-list">
-    <li class="social"><a href=""><img src="<?= get_template_directory_uri().'/dist/assets/img/facebook.png' ?>" alt="Facebok"></a></li>
-    <li class="social"><a href=""><img src="<?= get_template_directory_uri().'/dist/assets/img/gorjeo.png' ?>" alt="Twitter"></a></li>
-    <li class="social"><a href=""><img src="" alt="No se"></a></li>
+    <li class="social"><a href=""><img class="picture-social" src="<?= get_template_directory_uri().'/dist/assets/img/facebook.png' ?>" alt="Facebok"></a></li>
+    <li class="social"><a href=""><img class="picture-social" src="<?= get_template_directory_uri().'/dist/assets/img/gorjeo.png' ?>" alt="Twitter"></a></li>
+    <li class="social"><a href=""><img class="picture-social" src="" alt="No se"></a></li>
 </ul>
