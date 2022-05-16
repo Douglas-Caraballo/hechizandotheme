1345148134b89d910bdad5dd4f58ede152a9b0bc
Doug <karaballo.douglas@hotmail.com>
2022-05-16 13:58:45 -0400

    Cambios a los estilos del archive en el area del autor

--- template-parts/components/archive/element-archive.php
+++ template-parts/components/archive/element-archive.php
@@ -7,7 +7,7 @@
     <div class="archive-item__info">
         <span><?php the_title('<h3 class="archive-title-post"><a href='.get_the_permalink().'>','</a></h3>'); ?></span>
         <div class= "archive-item__info__detalist">
-            <span><h5><?php the_author(); ?></h5></span>
+            <span><h5><a class="archive-item__info__detalist__author" href="<?= esc_url( get_author_posts_url( get_the_author_meta('ID'))); ?>"><?php the_author(); ?></a></h5></span>
             <span><h5><?php echo get_the_date('M d, Y'); ?></h5></span>
         </div>
         <div class="archive-item__info__excerpt">
